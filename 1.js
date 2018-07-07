523601be81af4f10031a24eb2546ccf4922687fa
acer <saiakhilallam9@gmail.com>
2018-07-07 15:55:55 +0530

    final

--- 1.js
+++ 1.js
@@ -10,9 +10,19 @@ function getFile(file,callback){
     };
     xhr.send(null);
 }
+
 getFile("1.json",function(text){
- let data=JSON.parse(text);
- console.log(data);
+ var data=JSON.parse(text);
+ 	career(data.person);
 
 }
 )
+
+function career(text){
+    console.log(text);
+    // document.getElementById("akhil").innerHTML = text.education.inter.cname;
+    document.getElementById("carobj").innerHTML="<h3>" +text.career.obj+ "</h3>"
+    document.getElementById("add").innerHTML="<h2>" +text.address.street+ " "+text.address.village+"</h2>"
+    document.getElementById("qual").innerHTML="<li>"+text.education.school.sname +" "+text.education.school.year +"</li>"+"<li>"+text.education.inter.cname +"</li>"+"<li>"+text.education.engineering.cname +"</li>"
+        
+}
